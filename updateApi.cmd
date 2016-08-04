3f3008f43e5575d04552fde776be77674bf8c1c5
Qinen Zhu <qinzhu@microsoft.com>
2016-08-04 13:07:35 +0800

    Update CI script

--- updateApi.cmd
+++ updateApi.cmd
@@ -1,9 +1,7 @@
-REM clear azure-sdk-for-net/ and api/
-rmdir /s /q "azure-sdk-for-net"
-rmdir /s /q "api""
-
-REM git clone azure .NET SDK souce code
-git clone "https://github.com/Azure/azure-sdk-for-net.git"
-
-REM run docfx
 docfx metadata docfx.json
+
+git config user.email "vscopbld@microsoft.com"
+git config user.name "OpenPublishBuild"
+git add .
+git commit -m "CI Updates"
+git push -u origin master
