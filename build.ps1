0155cabb6a3aafcfed3eb46b20dc9b8706cb101e
Qinen Zhu <qinzhu@microsoft.com>
2016-08-26 17:11:37 +0800

    Add build.ps1 & remove updateApi.cmd

---
+++ build.ps1
@@ -0,0 +1,35 @@
+$ErrorActionPreference = 'Stop'
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptHome = Split-Path $scriptPath
+
+Push-Location $scriptHome
+
+$azureDoc = "azure-docs-sdk-dotnet"
+$docfxZip = "docfx.zip"
+$docfx = "docfx"
+$docfxExeUrl = "https://github.com/dotnet/docfx/releases/download/v2.1.0-cli-alpha/docfx.cli.zip"
+
+# download docfx from github.com
+Invoke-WebRequest $docfxExeUrl -OutFile $docfxZip
+
+# unzip docfx.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip $docfxZip $docfx
+
+# run docfx metadata to generate YAML metadata
+& .\$docfx\docfx.exe metadata $azureDoc\docfx.json
+if($LASTEXITCODE -ne 0)
+{
+    Pop-Location
+    exit 1
+}
+
+Pop-Location
