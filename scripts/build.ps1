99e42a8c0beee5128686de060898c5974246c141
Qinen Zhu <qinzhu@microsoft.com>
2016-10-10 17:28:39 +0800

    move step intalling docfx from appveyor.yml to build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,46 +4,33 @@ $scriptPath = $MyInvocation.MyCommand.Path
 $rootFolder = Split-Path $scriptPath | Split-Path
 
 Push-Location $rootFolder
-md -Force "docfx"
-$docfxZip = "docfx\docfx.zip"
-$docfx = "docfx\"
-$docfxExeUrl = "https://github.com/dotnet/docfx/releases/download/v2.1.0-cli-alpha/docfx.cli.zip"
 
-# download docfx from github.com
-Invoke-WebRequest $docfxExeUrl -OutFile $rootFolder\$docfxZip
-
-# unzip docfx.zip
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+function ProcessLastExitCode {
+    param($exitCode, $msg)
+    if ($lastexitcode -ne 0)
+    {
+        Write-Error $msg + ", exit code: $lastexitcode"
+        Pop-Location
+        Exit 1
+    }
 }
 
-Unzip $rootFolder\$docfxZip $rootFolder\$docfx
+# install latest docfx
+& cinst docfx
+ProcessLastExitCode($lastexitcode, "failed to install latest docfx")
 
 # clean api_ref
 Remove-Item "api_ref\*" -Force -recurse
 
 # run docfx metadata to generate YAML metadata
-& $docfx\docfx.exe metadata
-if($LASTEXITCODE -ne 0)
-{
-    Pop-Location
-    exit 1
-}
+& docfx metadata
+ProcessLastExitCode($lastexitcode, "failed to run docfx metadata")
 
 # merge toc.yml if needed
 if(Test-Path scripts/mergeToc.js)
 {
     & node scripts/mergeToc.js
-    if($LASTEXITCODE -ne 0)
-    {
-        Pop-Location
-        exit 1
-    }
+    ProcessLastExitCode($lastexitcode, "failed to merge toc.yml")
 }
-
 
 Pop-Location
