7b8a4aa1b286f73daa95ddb5bab7194572326a28
Qinen Zhu <qinzhu@microsoft.com>
2016-10-26 16:24:08 +0800

    Update build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -7,7 +7,7 @@ Push-Location $rootFolder
 
 function ProcessLastExitCode {
     param($exitCode, $msg)
-    if ($exitCode.Equals(0))
+    if ($exitCode -ne 0)
     {
         Write-Error "$msg, exit code: $exitCode"
         Pop-Location
@@ -17,20 +17,20 @@ function ProcessLastExitCode {
 
 # install latest docfx
 & cinst docfx
-ProcessLastExitCode($lastexitcode, "failed to install latest docfx")
+ProcessLastExitCode $lastexitcode "failed to install latest docfx"
 
 # clean api_ref
 Remove-Item "api-ref\*" -Force -recurse
 
 # run docfx metadata to generate YAML metadata
 & docfx metadata
-ProcessLastExitCode($lastexitcode, "failed to run docfx metadata")
+ProcessLastExitCode $lastexitcode "failed to run docfx metadata"
 
 # merge toc.yml if needed
 if(Test-Path scripts/mergeToc.js)
 {
     & node scripts/mergeToc.js
-    ProcessLastExitCode($lastexitcode, "failed to merge toc.yml")
+    ProcessLastExitCode $lastexitcode "failed to merge toc.yml"
 }
 
 Pop-Location
